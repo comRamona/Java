11e9e8cc748702c417995e23add183cab626d9f6
comRamona <com.ramona@yahoo.com>
2015-03-21 00:29:17 +0000

    Mock exam 11-12 solutions

---
+++ Mock11-12/src/Element.java
@@ -0,0 +1,19 @@
+
+public class Element {
+	 private final String element;
+	 private final int atomicNum;
+	 private final String sym;
+	 private final double weight;
+	 public Element(String element, int atomicNum,String sym, double weight){
+		 this.element=element;
+		 this.atomicNum=atomicNum;
+		 this.sym=sym;
+		 this.weight=weight;
+		 
+	 }
+	 public String getElement(){ return element; }
+	 public int getAtomicNum(){ return atomicNum; }
+	 public String getSym(){ return sym; }
+	 public double getWeight(){return weight;}
+	 public String toString() {return "Element("+element+","+atomicNum+","+sym+","+weight+")";}
+}

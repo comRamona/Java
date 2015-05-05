27593a2094ccca5ea0b3b23bcd90536f12655979
Rami <com.ramona@yahoo.com>
2015-05-05 17:43:51 +0100

    playing with buffers

--- Labs/Lab7/ReadFIles/src/ReadFile.java
+++ Labs/Lab7/ReadFIles/src/ReadFile.java
@@ -8,7 +8,7 @@ public class ReadFile {
 	} 
 	In file = new In("myfile.txt");
 	String s = file.readAll();
-	String[] words = s.split(" ");
+	String[] words = s.split("\\W+");  //non-word character
 	for(int i=0;i<words.length;i++)
 		System.out.println(words[i]);
 	

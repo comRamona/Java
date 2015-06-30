0af9039ad5a4bb9668bb478106e5e40e8cbc51fe
Ramona Comanescu <com.ramona@yahoo.com>
2015-06-30 13:23:57 +0300

    Create ReverseWords.java

---
+++ Algorithms/Short Exercises/ReverseWords.java
@@ -0,0 +1,23 @@
+//this programs reverse every word in a gien string and leaves non-alphabetic characters in place
+
+public class ReverseWords{
+public static String reverse(String str){     
+        String temp = "", result = "";
+        for(int i=0;i<str.length();i++)
+            if ( (Character.isAlphabetic(str.charAt(i))))
+                temp = str.charAt(i) + temp;        //reverse word
+            else {
+                result += temp + str.charAt(i);      //punctuation mark, copy reversed word and start over to revert the next one
+                temp = "";
+            }
+
+        result += temp;  //last word nneds to be copied
+        System.out.println(result);
+        return result;   
+    }
+public static void main(String[] args){
+	String s1=reverse("Hello World, hello nation!");
+	String s2="olleH dlroW, olleh noitan!";  //expected output
+	System.out.println(s1.equals(s2));  //check expected output and actual output match
+}
+}

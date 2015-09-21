aaae855aa3093d015f064d18f02be2be226fe61c
Rami <com.ramona@yahoo.com>
2015-09-21 13:57:33 +0100

    SRM 648 Div1 250

---
+++ TopCoder/src/AB.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+//SRM 648 Div1 250
+public class AB{
+    public String createString(int N,int K){
+   StringBuilder sb=new StringBuilder(N);
+		for (int i = 0; i < N; i++)
+			sb.append('B'); 
+		if (K > (N * N) / 4)
+			return "";
+		if (K == 0)
+		{
+			return sb.toString();
+			}
+		int nrA = 1;
+		while (nrA <= N / 2) {
+			int max = (N - nrA) * nrA;
+			if(K <= max) {
+				int pos = 0;
+				for (int p = 0; p <= N - nrA; p++)
+					if (K == ((N - nrA - p) * nrA)) {
+						pos = p;
+						for (int j = pos; j < pos+nrA; j++)
+							sb.setCharAt(j,'A');
+						return sb.toString();
+					}
+			}
+			nrA++;
+		}
+		return "";
+
+	}
+	
+	public static void main(String[] args) {
+		long x1=System.currentTimeMillis();
+		String s = new AB().createString(40, 100);
+		long x2=(System.currentTimeMillis());
+		System.out.println(x2-x1);
+		System.out.println(s);
+	
+	}
+    
+}

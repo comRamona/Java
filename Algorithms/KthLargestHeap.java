c5267e9a9734a2f61ea7788f8093713c60d837af
Ramona Comanescu <com.ramona@yahoo.com>
2016-06-13 18:31:18 +0100

    Create KthLargestHeap.java

---
+++ Algorithms/KthLargestHeap.java
@@ -0,0 +1,14 @@
+public class KthLargestHeap{
+public int findKthLargest(int[] nums, int k) {
+    PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+    for(int i: nums){
+        q.offer(i);
+ 
+        if(q.size()>k){
+            q.poll();
+        }
+    }
+ 
+    return q.peek();
+}
+}
